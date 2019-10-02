dc57c9cd45d1ee75a94746f58f2e3e3360128ce9
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-10-02 12:52:19 +0200

    Moved display functions to extention methods

---
+++ DungeonGenerator/Maze2D/Utils/Displayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGenerator.Maze2D.Utils
+{
+    public static class Displayer
+    {
+        public static void Display(this bool[,] maze, List<Position> path)
+        {
+            for (int i = 0; i < maze.GetLength(0) + 1; i++)
+                Console.Write('x');
+            Console.WriteLine();
+
+            for (int i = 0; i < maze.GetLength(1); i++)
+            {
+                Console.Write('x');
+                for (int j = 0; j < maze.GetLength(0); j++)
+                {
+                    if (path.FindIndex(p => p.X == j / 2 && p.Y == i / 2) >= 0)
+                    {
+                        if (maze[j, i])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write('^');
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write('x');
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write((maze[j, i]) ? ' ' : 'x');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Display(this bool[,] maze)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < maze.GetLength(0) + 1; i++)
+                Console.Write('x');
+            Console.WriteLine();
+
+            for (int i = 0; i < maze.GetLength(1); i++)
+            {
+                Console.Write('x');
+                for (int j = 0; j < maze.GetLength(0); j++)
+                    Console.Write((maze[j, i]) ? ' ' : 'x');
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- DungeonGenerator/Program.cs
+++ DungeonGenerator/Program.cs
@@ -30,37 +30,7 @@ namespace DungeonGenerator
             var path = dijkstra.FindPath(m, new Position(0, 0), new Position(m.Width - 1, m.Height - 1));
 
             var maze = m.ToBoolArray();
-
-            for (int i = 0; i < m.Width * 2 + 1; i++)
-                Console.Write('x');
-            Console.WriteLine();
-
-            for (int i = 0; i < maze.GetLength(1); i++)
-            {
-                Console.Write('x');
-                for (int j = 0; j < maze.GetLength(0); j++)
-                {
-                    if (path.FindIndex(p => p.X == j / 2 && p.Y == i /2) >= 0)
-                    {
-                        if(maze[j,i])
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write('^');
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.Write('x');
-                        }
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.Write((maze[j, i]) ? ' ' : 'x');
-                    }
-                }
-                Console.WriteLine();
-            }
+            maze.Display(path);
         }
     }
 }
